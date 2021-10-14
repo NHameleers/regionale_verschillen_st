58b5b4cb9ad31b9889761d1b25a76d4f2a144d6d
Hameleers <Niels.Hameleers@unimaas.nl>
2021-10-14 11:23:09 +0200

    teveel aanpassingen voor 1 commit

--- app.py
+++ app.py
@@ -1,5 +1,101 @@
+import geopandas
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import numpy as np
 import pandas as pd
 import streamlit as st
 
-ggd_topo = alt.topo_feature('ggd_regios.geojson', 'features')
+# ggd_topo = alt.topo_feature('ggd_regios.geojson', 'features')
 
+
+### GEO DATA
+gdf_raw = (geopandas.read_file("ggd_regios.shp").rename(columns={'statnaam': 'ggd_regio'}))
+
+# 'GDF raw'
+# st.write(gdf_raw)
+
+
+
+### ANALYSIS DATA
+df = pd.read_csv('simulated_ggd_regio_data.csv')
+
+# 'DF'
+# st.write(df.head())
+
+
+
+### USER INPUT
+uitkomstmaat = st.selectbox('Selecteer Uitkomstmaat:', df.uitkomstmaat.unique())
+ref_regio = st.selectbox('Selecteer referentie GGD regio:', df.ggd_regio.unique())
+
+
+
+### DATA FOR PLOTTING PREPARATION BASED ON INPUTS
+verschil_M1 = df.loc[(df.uitkomstmaat == uitkomstmaat) & (df.referentie_regio == ref_regio) & (df.model == 1), ['ggd_regio', 'verschil']]
+verschil_M1 = verschil_M1.rename(columns={'verschil': 'verschil_M1'})
+
+verschil_M6 = df.loc[(df.uitkomstmaat == uitkomstmaat) & (df.referentie_regio == ref_regio) & (df.model == 6), ['ggd_regio', 'verschil']]
+verschil_M6 = verschil_M6.rename(columns={'verschil': 'verschil_M6'})
+
+gdf = gdf_raw.merge(verschil_M1, on='ggd_regio', how='left').merge(verschil_M6, on='ggd_regio', how='left')
+
+# 'GDF'
+# st.write(gdf)
+
+
+
+
+
+
+
+
+### PLOTTING
+
+# Center of colormap should be zero
+divnorm=colors.TwoSlopeNorm(vmin=df.loc[(df.uitkomstmaat == uitkomstmaat), 'verschil'].min(), vcenter=0, vmax=df.loc[(df.uitkomstmaat == uitkomstmaat), 'verschil'].max())
+
+fig1, ax1 = plt.subplots()
+
+divider1 = make_axes_locatable(ax1)
+cax1 = divider1.append_axes("bottom", size="5%", pad=0.1)
+
+ax1 = gdf.plot(column='verschil_M1',
+                ax=ax1,
+                cmap='cividis',
+                legend=True,
+                cax=cax1,
+                legend_kwds={'label': f'Verschil {uitkomstmaat}\n met regio: {ref_regio}',
+                            'orientation': "horizontal"},
+                norm=divnorm)
+ax1.set_axis_off()
+# fig1.set_facecolor('black')
+
+
+fig2, ax2 = plt.subplots()
+divider2 = make_axes_locatable(ax2)
+cax2 = divider2.append_axes("bottom", size="5%", pad=0.1)
+
+ax2 = gdf.plot(column='verschil_M6',
+                ax=ax2,
+                cmap='cividis',
+                legend=True,
+                cax=cax2,
+                legend_kwds={'label': f'Verschil {uitkomstmaat}\nmet regio: {ref_regio}',
+                            'orientation': "horizontal"},
+                norm=divnorm)
+ax2.set_axis_off()
+# fig2.set_facecolor('black')
+
+
+# '## Hieronder 2 figuren, blabla'
+# '## Links zonder correctie, rechts na (met?) correctie voor demografie, SES, eenzaamheid, leefstijl, ervaren gezondheid etc (TODO)'
+
+col1, col2 = st.columns(2)
+col1.subheader(f'Verschillen in {uitkomstmaat} zonder correctie')
+col1.pyplot(fig1)
+col2.subheader(f'Verschillen in {uitkomstmaat} na correctie')
+col2.pyplot(fig2)
+
+'# DISCLAIMER DISCLAIMER!!!'
+'# DEZE DATA ZIJN GESIMULEERD!'
