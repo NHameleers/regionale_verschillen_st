4d3d068ebfb7e8b75cdfa3c41c2e2388d56184d5
Hameleers <Niels.Hameleers@unimaas.nl>
2021-10-20 00:51:22 +0200

    Now overwriting theme config correctly via setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[theme]\
+primaryColor='#F63366'\
+backgroundColor='#FFFFFF'\
+secondaryBackgroundColor='#F0F2F6'\
+textColor='#262730'\
+font='sans serif'\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
