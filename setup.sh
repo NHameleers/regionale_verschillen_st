0e8bb7e9dc6427915602f3cb492c8c92cb963ec0
Hameleers <Niels.Hameleers@unimaas.nl>
2021-10-21 18:55:03 +0200

    fixed typo in setup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "[theme]\n\
 primaryColor='#F63366'\n\
 backgroundColor='#FFFFFF'\n\
 secondaryBackgroundColor='#F0F2F6'\n\
-textColor#262730'\n\
+textColor='#262730'\n\
 font='sans serif'\n\
 [server]\n\
 port = $PORT\n\
