0e8b7cf0473e75c540db6d4eff269de152e5f4ce
Hameleers <Niels.Hameleers@unimaas.nl>
2021-10-21 17:29:00 +0200

    sample van kosten data als input

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
 import pandas as pd
 import streamlit as st
-
+st.set_page_config(layout="wide")
 
 
 cmap = plt.cm.coolwarm  # define the colormap
@@ -17,7 +17,11 @@ binnedcmap = colors.ListedColormap(newmapcolors)
 
 
 beschrijving_uitkomstmaat = {'Gezondheid Algemeen': f'Percentage volwassenen (19 jaar en ouder) die een minder goede gezondheid ervaren',
-'Totale Zorgkosten': 'totale zorgkosten in basisverzekering'}
+'TotaleZorgkosten': 'totale zorgkosten in basisverzekering',
+'nopzvwkhuisartsconsult': 'Kosten huisarts consult basisverzekering',
+'GGZkosten': 'Kosten GGZ basisverzekering',
+'zvwkziekenhuis': 'Kosten medisch-specialistische zorg basisverzekering',
+'zvwkfarmacie': 'Kosten farmacie basisverzekering'}
 
 
 
@@ -25,7 +29,7 @@ beschrijving_uitkomstmaat = {'Gezondheid Algemeen': f'Percentage volwassenen (19
 
 
 ### GEO DATA
-gdf_raw = (geopandas.read_file("ggd_regios.shp").rename(columns={'statnaam': 'ggd_regio'}))
+gdf_raw = (geopandas.read_file("ggd_regios2.shp").rename(columns={'statnaam': 'ggd_regio'}))
 
 # 'GDF raw'
 # st.write(gdf_raw)
@@ -33,12 +37,14 @@ gdf_raw = (geopandas.read_file("ggd_regios.shp").rename(columns={'statnaam': 'gg
 
 
 ### ANALYSIS DATA
-df = pd.read_csv('simulated_ggd_regio_data.csv')
+df = pd.read_csv('final_input_voor_tool.csv')
 
 # 'DF'
 # st.write(df.head())
 
+
 '# Regionale gezondheidsverschillen'
+
 
 ### USER INPUT
 uitkomstmaat = st.selectbox('Selecteer Uitkomstmaat:', df.uitkomstmaat.unique())
@@ -47,14 +53,16 @@ ref_regio = st.selectbox('Selecteer referentie GGD regio:', df.ggd_regio.unique(
 
 
 ### DATA FOR PLOTTING PREPARATION BASED ON INPUTS
-verschil_M1 = df.loc[(df.uitkomstmaat == uitkomstmaat) & (df.referentie_regio == ref_regio) & (df.model == 1), ['ggd_regio', 'verschil']]
+verschil_M1 = df.loc[(df.uitkomstmaat == uitkomstmaat) & (df.referentie_regio == ref_regio) & (df.model == 'M1'), ['ggd_regio', 'verschil']].copy()
 verschil_M1 = verschil_M1.rename(columns={'verschil': 'verschil_M1'})
 
-verschil_M6 = df.loc[(df.uitkomstmaat == uitkomstmaat) & (df.referentie_regio == ref_regio) & (df.model == 6), ['ggd_regio', 'verschil']]
+verschil_M6 = df.loc[(df.uitkomstmaat == uitkomstmaat) & (df.referentie_regio == ref_regio) & (df.model == 'M6'), ['ggd_regio', 'verschil']].copy()
 verschil_M6 = verschil_M6.rename(columns={'verschil': 'verschil_M6'})
 
 gdf = gdf_raw.merge(verschil_M1, on='ggd_regio', how='left').merge(verschil_M6, on='ggd_regio', how='left')
 
+
+# st.write([reg for reg in df.ggd_regio if reg not in gdf.ggd_regio])
 # 'GDF'
 # st.write(gdf)
 
@@ -72,7 +80,7 @@ st.write(titeltekst)
 # Center of colormap should be zero
 divnorm=colors.TwoSlopeNorm(vmin=df.loc[(df.uitkomstmaat == uitkomstmaat), 'verschil'].min(), vcenter=0, vmax=df.loc[(df.uitkomstmaat == uitkomstmaat), 'verschil'].max())
 
-fig1, ax1 = plt.subplots()
+fig1, ax1 = plt.subplots(figsize=(6,6))
 
 divider1 = make_axes_locatable(ax1)
 cax1 = divider1.append_axes("bottom", size="5%", pad=0.1)
@@ -91,7 +99,7 @@ ax1.set_axis_off()
 # fig1.set_facecolor('black')
 
 
-fig2, ax2 = plt.subplots()
+fig2, ax2 = plt.subplots(figsize=(6,6))
 divider2 = make_axes_locatable(ax2)
 cax2 = divider2.append_axes("bottom", size="5%", pad=0.1)
 
@@ -112,23 +120,20 @@ ax2.set_axis_off()
 # '## Hieronder 2 figuren, blabla'
 # '## Links zonder correctie, rechts na (met?) correctie voor demografie, SES, eenzaamheid, leefstijl, ervaren gezondheid etc (TODO)'
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns((1, 1, 2))
+col3.write(f'''### Leeswijzer
+De kaarten tonen de percentages (**TODO**: Dynamisch maken voor kosten) van de volwassenen (19 jaar en ouder) die {beschrijving_uitkomstmaat[uitkomstmaat]} hebben per regio. Hoe hoger het percentage, hoe meer volwassen {beschrijving_uitkomstmaat[uitkomstmaat]} hebben, hoe roder de regio kleurt. Grijze regio’s verschillen niet met de gekozen referentieregio {ref_regio}. De eerste kaart geeft de cijfers weer zonder enige correctie. De cijfers in het tweede kaartje zijn gecorrigeerd voor leeftijd, geslacht, burgerlijke staat, migratieachtergrond, huishoudinkomen, opleidingsniveau, moeite met rondkomen, BMI, roken, alcoholconsumptie, voldoende beweging, eenzaamheid en zelfregie. Gemiddeld heeft **TODO**% van de Nederlanders {beschrijving_uitkomstmaat[uitkomstmaat]} voor correctie en **TODO**% na correctie.
+''')
 col1.write(f'Voor correctie')
 col1.pyplot(fig1)
 col2.write(f'Na correctie')
 col2.pyplot(fig2)
 
 
-f'''### Leeswijzer
-
-De kaarten tonen de percentages (**TODO**: Dynamisch maken voor kosten) van de volwassenen (19 jaar en ouder) die {beschrijving_uitkomstmaat[uitkomstmaat]} hebben per regio. Hoe hoger het percentage, hoe meer volwassen {beschrijving_uitkomstmaat[uitkomstmaat]} hebben, hoe roder de regio kleurt. Grijze regio’s verschillen niet met de gekozen referentieregio {ref_regio}. De eerste kaart geeft de cijfers weer zonder enige correctie. De cijfers in het tweede kaartje zijn gecorrigeerd voor leeftijd, geslacht, burgerlijke staat, migratieachtergrond, huishoudinkomen, opleidingsniveau, moeite met rondkomen, BMI, roken, alcoholconsumptie, voldoende beweging, eenzaamheid en zelfregie. Gemiddeld heeft **TODO**% van de Nederlanders {beschrijving_uitkomstmaat[uitkomstmaat]} voor correctie en **TODO**% na correctie.
-'''
-
-
 
 
 '''### Bronverantwoording
-(link naar pagina met uitleg en categorisering, bron etc per variabele en uitkomstmaat)
+(Uitvouwding naar pagina met uitleg en categorisering, bron etc per variabele en uitkomstmaat)
 '''
 
 ''
--- setup.sh
+++ setup.sh
@@ -4,10 +4,11 @@ echo "[theme]\n\
 primaryColor='#F63366'\n\
 backgroundColor='#FFFFFF'\n\
 secondaryBackgroundColor='#F0F2F6'\n\
-textColor='#262730'\n\
+textColor#262730'\n\
 font='sans serif'\n\
 [server]\n\
-headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
 " > ~/.streamlit/config.toml
